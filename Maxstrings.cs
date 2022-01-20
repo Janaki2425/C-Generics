5ff126c56262fc620c36d1962febd1c972bd6223
Janu shankar <janakiharishankar24@gmail.com>
2022-01-21 00:51:32 +0530

    Maximum among strings

---
+++ Maxstrings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Maxstrings
+    {
+        public static string Maximumstring(string firststring, string secondstring, string thirdstring)
+        {
+            if (firststring.CompareTo(secondstring) > 0 && firststring.CompareTo(thirdstring) > 0 ||
+                    firststring.CompareTo(secondstring) >= 0 && firststring.CompareTo(thirdstring) > 0 ||
+                firststring.CompareTo(secondstring) > 0 && secondstring.CompareTo(thirdstring) >= 0)
+            {
+                return firststring;
+            }
+
+            if (secondstring.CompareTo(firststring) > 0 && secondstring.CompareTo(thirdstring) > 0 ||
+                    secondstring.CompareTo(firststring) >= 0 && secondstring.CompareTo(thirdstring) > 0 ||
+                secondstring.CompareTo(firststring) > 0 && secondstring.CompareTo(thirdstring) >= 0)
+            {
+                return secondstring;
+            }
+
+            if (thirdstring.CompareTo(firststring) > 0 && thirdstring.CompareTo(secondstring) > 0 ||
+                    thirdstring.CompareTo(firststring) >= 0 && thirdstring.CompareTo(secondstring) > 0 ||
+                thirdstring.CompareTo(firststring) > 0 && thirdstring.CompareTo(secondstring) >= 0)
+            {
+                return thirdstring;
+            }
+            return firststring;
+
+
+        }
+    }
+}
