2e6b85d31a5b0c24eb14dc6eb6eabbb28f6b4a7a
Janu shankar <janakiharishankar24@gmail.com>
2022-01-21 01:39:30 +0530

    initial main program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Generics;
+
+public class Program
+
+{
+    public static void Main(String[] args)
+    {
+        Maxnos maxnosUC1 = new Maxnos();
+        maxnosUC1.Maximumvalue();
+        MaxFloats maxfloatsUC2 = new MaxFloats();
+        maxfloatsUC2.Maximumfloat();
+        Maxstrings maxstringsUC3 = new Maxstrings();
+        maxstringsUC3.Maximumstring();
+        RefactorGenericMethod<T> refactorGenericMethod = new();
+        refactorGenericMethod.Comparevalues();
+        RefactorGenericClass<T> refactorGenericClass = new();
+        refactorGenericClass.Comparevalue();
+        refactorGenericClass.Maxmethod();
+
+
+
+            
+
+
+    }
+    
+}
+
+internal class T
+{
+}
